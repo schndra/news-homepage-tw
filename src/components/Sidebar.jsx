4e045bda57cb4bc2325129f5de9a5d102e5455a2
Abhishek Sachindra <73639997+schndra@users.noreply.github.com>
2022-10-30 19:02:21 +0530

    completed sidebar styling

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import CloseButton from "../assets/images/icon-menu-close.svg";
+import { links } from "../data";
 
 const Sidebar = () => {
-  return <aside>Sidebar</aside>;
+  return (
+    <>
+      <aside className="fixed grid  grid-flow-row gap-y-4 top-0 right-0 bg-nOffWhite w-[325px] h-full z-10 ">
+        <button className="fixed top-8 right-8">
+          <img src={CloseButton} alt="close-btn" />
+        </button>
+        <ul className="row-start-2 row-span-2 pl-8">
+          {links.map((link) => {
+            const { id, url, text } = link;
+            return (
+              <li key={id} className="mb-6">
+                <a href={url} className="hover:text-primarySoftRed">
+                  {text}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </aside>
+    </>
+  );
 };
 
 export default Sidebar;
