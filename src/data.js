47a2fc66c1357aba82bff36b4772c06f2b763f68
Abhishek Sachindra <73639997+schndra@users.noreply.github.com>
2022-10-30 12:10:28 +0530

    project init -configured tailwindcss -added image assets

---
+++ src/data.js
@@ -0,0 +1,7 @@
+export const links = [
+  { id: 1, url: "/", text: "Home" },
+  { id: 2, url: "/new", text: "New" },
+  { id: 3, url: "/popular", text: "Popular" },
+  { id: 4, url: "/trending", text: "Trending" },
+  { id: 5, url: "/categories", text: "Categories" },
+];
