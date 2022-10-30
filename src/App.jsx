fa039fe5e8c109f93b19e077cceb98bad7a3acc2
Abhishek Sachindra <73639997+schndra@users.noreply.github.com>
2022-10-30 19:00:25 +0530

    changed to display only home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { useState } from "react";
 import Home from "./components/Home";
-import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <Navbar />
-      {/* <Home /> */}
+      <Home />
     </>
   );
 }
