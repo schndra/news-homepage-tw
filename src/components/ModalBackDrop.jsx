677aa8f76281ab82a036462a4715f2ab6e1af3a5
Abhishek Sachindra <73639997+schndra@users.noreply.github.com>
2022-10-30 19:02:05 +0530

    completed  modal backdrop blur effect

---
+++ src/components/ModalBackDrop.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ModalBackDrop = () => {
+  return (
+    <div className="fixed w-full h-full bg-nVeryDarkBlue opacity-25 "></div>
+  );
+};
+
+export default ModalBackDrop;
