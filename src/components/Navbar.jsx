543b4e28d254bc92050ad05817a179d75407958f
Abhishek Sachindra <73639997+schndra@users.noreply.github.com>
2022-10-30 12:14:19 +0530

    completed Navbar - used map method to display nav links taken from data.js

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import Logo from "../assets/images/logo.svg";
+import IconMenu from "../assets/images/icon-menu.svg";
+import { links } from "../data";
+console.log(links);
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="w-vw mx-auto md:flex md:items-center md:justify-between xl:w-mFixed">
+        <div className="w-vw p-4 mx-auto flex justify-between items-center ">
+          <img src={Logo} alt="logo" />
+          <button className="md:hidden">
+            <img src={IconMenu} alt="logo" />
+          </button>
+        </div>
+        <div className="hidden md:block">
+          <ul className="flex gap-9">
+            {links.map((link) => {
+              const { id, text, url } = link;
+              return (
+                <li
+                  key={id}
+                  className="hover:text-primarySoftRed cursor-pointer"
+                >
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+            {/* <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">New</a>
+            </li>
+            <li>
+              <a href="#">Popular</a>
+            </li>
+            <li>
+              <a href="#">Trending</a>
+            </li>
+            <li>
+              <a href="#">Categories</a>
+            </li> */}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
